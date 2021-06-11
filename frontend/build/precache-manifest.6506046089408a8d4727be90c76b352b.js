15b8ead23ae38acf88fd8b023a8db15fe1f726d7
bhawesh2009 <bpaliwal1980@gmail.com>
2021-06-11 13:26:14 +0530

    bhawesh

---
+++ frontend/build/precache-manifest.6506046089408a8d4727be90c76b352b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657a250da49edade6446c57f29f6d102",
+    "url": "/bhawesh2009/marbleworld/index.html"
+  },
+  {
+    "revision": "320d6b500bd29717e777",
+    "url": "/bhawesh2009/marbleworld/static/css/2.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "3153aeb482ae72647854",
+    "url": "/bhawesh2009/marbleworld/static/css/main.36f4fc97.chunk.css"
+  },
+  {
+    "revision": "320d6b500bd29717e777",
+    "url": "/bhawesh2009/marbleworld/static/js/2.d62826a8.chunk.js"
+  },
+  {
+    "revision": "dff38bf37fc9eda4acb236ebba3264fd",
+    "url": "/bhawesh2009/marbleworld/static/js/2.d62826a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3153aeb482ae72647854",
+    "url": "/bhawesh2009/marbleworld/static/js/main.ebc8d8f2.chunk.js"
+  },
+  {
+    "revision": "0ec9f499582451352520",
+    "url": "/bhawesh2009/marbleworld/static/js/runtime-main.75e1a073.js"
+  }
+]);
